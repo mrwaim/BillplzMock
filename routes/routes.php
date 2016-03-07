687de140c5d7465b0dff1ab9385476c67b5b78d0
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-07 18:33:04 +0800

    ns

--- routes/routes.php
+++ routes/routes.php
@@ -1,8 +1,8 @@
 <?php
 Route::group(['prefix' => 'billplz-mock'], function () {
-    Route::any('collections', 'BillplzMockController@collections');
-    Route::any('bills', 'BillplzMockController@bills');
-    Route::any('view-bill/{collectionId}/{email}/{name}/{phone}/{amount}/{order_id}', 'BillplzMockController@viewBill');
-    Route::post('pay-amount', 'BillplzMockController@payAmount');
-    Route::post('decline-amount', 'BillplzMockController@declineAmount');
+    Route::any('collections', '\Klsandbox\BillplzMock\Http\Controllers\BillplzMockController@collections');
+    Route::any('bills', '\Klsandbox\BillplzMock\Http\Controllers\BillplzMockController@bills');
+    Route::any('view-bill/{collectionId}/{email}/{name}/{phone}/{amount}/{order_id}', '\Klsandbox\BillplzMock\Http\Controllers\BillplzMockController@viewBill');
+    Route::post('pay-amount', '\Klsandbox\BillplzMock\Http\Controllers\BillplzMockController@payAmount');
+    Route::post('decline-amount', '\Klsandbox\BillplzMock\Http\Controllers\BillplzMockController@declineAmount');
 });
